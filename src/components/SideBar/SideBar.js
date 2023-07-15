1ddf96b1459bd98bff67c443488c243a189aeec0
Carlin <carlin.d.mitchell@gmail.com>
2023-07-14 19:20:22 -0500

    fixed android issue

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -9,7 +9,7 @@ window.onresize = (e) => {
   const { innerWidth } = e.target;
   if (innerWidth >= 800) {
     const sideBar = document.querySelector("#side-bar");
-    sideBar.className = "visible ";
+    sideBar.className = "visible";
   }
 };
 
