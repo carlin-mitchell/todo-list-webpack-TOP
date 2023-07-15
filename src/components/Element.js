4d6c883de3527db8ac3dbb0ecce11b35aabe46d4
Carlin <carlin.d.mitchell@gmail.com>
2023-07-15 08:38:21 -0500

    refactor Element

--- src/components/Element.js
+++ src/components/Element.js
@@ -2,7 +2,7 @@
  * This Function allows you to create a dom element with its initial attributes defined by a passed-in object
  * @param {string} typeStr the element string you would use in document.createElement()
  * @param {object} propsObj an object containing the initial properties/attributes you want the element to have
- * @returns {element} the a reference to the element created in memory
+ * @returns {Element} the a reference to the element created in memory
  */
 const Element = (typeStr, propsObj) => {
   return Object.assign(document.createElement(typeStr), propsObj);
