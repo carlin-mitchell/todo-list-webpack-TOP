51cf79436d7a9952ee37c01885556f8123834307
Carlin Mitchell <carlinmi@umn.edu>
2023-07-31 11:21:59 -0500

    deploy-demo

--- src/components/Todos/EditForm/SubmitEditButton.js
+++ src/components/Todos/EditForm/SubmitEditButton.js
@@ -1,9 +1,16 @@
 import Element from "../../Element";
 
+function editTodo(e) {
+  e.preventDefault();
+}
+
 // COMPONENT
 const SubmitEditButton = () => {
   const button = Element("button", {
     innerText: "Submit Edit",
+    onclick: function (e) {
+      editTodo(e);
+    },
   });
   return button;
 };
